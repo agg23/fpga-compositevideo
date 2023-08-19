e41402d0600226d6d6d883819865ecf20a5e376f
Adam Gastineau <adam@agg.im>
2023-08-19 12:41:27 -0700

    Playing with hitting voltage targets

--- scripts/solver.ts
+++ scripts/solver.ts
@@ -1,7 +1,15 @@
-const bitCount = 3;
+const bitCount = 2;
 
 // 75 Ohms built into the TV
 const seriesResistance = 75;
+// This is incorrect, black voltage is 0.3. However with 3 bits, it's hard to solve for output values between 0.3-1.0,
+// so we calculate from 0.0-1.0
+// const blackVoltage = 0.0;
+const blackVoltage = 0.3;
+const whiteVoltage = 1.0;
+
+const colorRangeTolerance = 0.1;
+
 const voltage = 3.3;
 
 const maxResistor = 2000;
@@ -18,22 +26,30 @@ type ResistorConfig =
     };
 
 // Do a full search over the searchspace (stepping by 10)
-// const config: ResistorConfig = {
-//   type: "allValues",
-//   increment: 10,
-// } as ResistorConfig;
-
-// Search for specific resistor values
 const config: ResistorConfig = {
-  type: "setValues",
-  values: [
-    10, 22, 47, 100, 150, 200, 220, 270, 330, 470, 510, 680, 1000, 2000, 2200,
-    3300, 4700, 5100,
-  ],
+  type: "allValues",
+  increment: 10,
 } as ResistorConfig;
 
-// Our voltages are 0-1V, and we want to solve for values >0
-const voltageLevels = Math.pow(2, 3);
+// Search for specific resistor values
+// const config: ResistorConfig = {
+//   type: "setValues",
+//   values: [
+//     10, 22, 47, 100, 150, 200, 220, 270, 330, 470, 510, 680, 1000, 2000, 2200,
+//     3300, 4700, 5100,
+//   ],
+// } as ResistorConfig;
+
+const numberOfVoltageLevels = Math.pow(2, bitCount);
+
+const voltageLevels: number[] = [];
+
+const voltageRange = whiteVoltage - blackVoltage;
+const voltageStepSize = voltageRange / (numberOfVoltageLevels - 1);
+for (let i = 0; i < numberOfVoltageLevels; i++) {
+  // Take the steps between 0-0.7 and shift them to be 0.3-1.0
+  voltageLevels.push(voltageStepSize * i + blackVoltage);
+}
 
 let bestMatchValues: number[] | undefined = undefined;
 let bestDiff = Number.MAX_SAFE_INTEGER;
@@ -98,39 +114,60 @@ const buildActiveBitEquation = (values: number[], activeBits: number) => {
 };
 
 const processValues = (variableValues: number[], childrenCount: number) => {
-  if (childrenCount === 0) {
-    // Base case, evaluate this last level
-    const results: number[] = [];
-
-    for (let i = 0; i < voltageLevels; i++) {
-      const result = buildActiveBitEquation(variableValues, i);
-
-      results.push(result);
-    }
-
-    results.sort((a, b) => a - b);
-
-    let totalDiff = 0;
-
-    for (let i = 0; i < voltageLevels; i++) {
-      const result = results[i];
-
-      // Compare result at this index to the intended voltage level at this index
-      const expectedLevel = i / (voltageLevels - 1);
-      const diff = Math.abs(result - expectedLevel);
-
-      totalDiff += diff;
-    }
-
-    if (totalDiff < bestDiff) {
-      bestDiff = totalDiff;
-      bestMatchValues = variableValues;
-    }
-  } else {
+  if (childrenCount !== 0) {
     // Continue recursing
     resistorIterator(variableValues, (values) => {
       processValues(values, childrenCount - 1);
     });
+
+    return;
+  }
+
+  // Base case, evaluate this last level
+  const results: number[] = [];
+
+  for (let i = 0; i < numberOfVoltageLevels; i++) {
+    const result = buildActiveBitEquation(variableValues, i);
+
+    results.push(result);
+  }
+
+  results.sort((a, b) => a - b);
+
+  let totalDiff = 0;
+
+  // Skip 0
+  for (let i = 1; i < numberOfVoltageLevels; i++) {
+    const result = results[i];
+
+    if (i == 1 && blackVoltage - result > colorRangeTolerance) {
+      // Reject this match, as it's too far out of black spec
+      return;
+    } else if (
+      i == numberOfVoltageLevels - 1 &&
+      result - whiteVoltage > colorRangeTolerance
+    ) {
+      // Reject this match, as it's too far out of white spec
+      return;
+    }
+
+    if (result < blackVoltage) {
+      // Manually score with a small error
+      totalDiff += 0.1;
+
+      continue;
+    }
+
+    // Compare result at this index to the intended voltage level at this index
+    const expectedLevel = voltageLevels[i];
+    const diff = Math.abs(result - expectedLevel);
+
+    totalDiff += diff;
+  }
+
+  if (totalDiff < bestDiff) {
+    bestDiff = totalDiff;
+    bestMatchValues = variableValues;
   }
 };
 
@@ -144,7 +181,13 @@ resistorIterator([], (values) => {
 console.log("Best matches:", bestMatchValues);
 console.log("Diff:", bestDiff);
 
-for (let i = 0; i < voltageLevels; i++) {
+if (bestMatchValues === undefined) {
+  console.log("No match found");
+
+  process.exit();
+}
+
+for (let i = 0; i < numberOfVoltageLevels; i++) {
   const result = buildActiveBitEquation(bestMatchValues, i);
 
   const activeResistors = bestMatchValues.filter((_, index) => {
